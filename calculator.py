6b7214452512a4448cadc90a638c918ad9c9ca35
vagrant <vagrant@vagrant.vm>
2020-10-01 04:54:08 +0000

    added mod, pow, and quit functions

--- calculator.py
+++ calculator.py
@@ -10,14 +10,18 @@ while True:
     expression = input("Write your equation here: ")
     expression_tokens = expression.split(" ")
 
+    if expression_tokens[0] == "q":
+        print("Quitting now")
+        break
 #variables
     operator = expression_tokens[0]
     num1 = expression_tokens[1]
-    
+        
     if len(expression_tokens) == 3:
         num2 = expression_tokens[2]
 
     #list of operations here:
+
     if operator == "+":
         answer = add(int(num1), int(num2))
     
@@ -36,7 +40,12 @@ while True:
     elif operator == "cube":
         answer = cube(int(num1))
     
+    elif operator == "pow":
+        answer = power(int(num1), int(num2))
     
-
+    elif operator == "mod":
+        answer = mod(int(num1), int(num2))
+    
+    #print the answer
     print(answer)
     
