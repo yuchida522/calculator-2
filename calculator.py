fe48c4764d520e3a899b2c3a4b74502c021c3fa1
vagrant <vagrant@vagrant.vm>
2020-10-01 03:59:11 +0000

    setting up input variables

--- calculator.py
+++ calculator.py
@@ -5,3 +5,12 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+expression = input("Write your equation here: ")
+expression_tokens = expression.split(" ")
+
+while True:
+    for i in range(len(expression_tokens)-1):
+        if expression_tokens[0] == "+":
+            num1 = expression_tokens[i + 1]
+            num2 = expression_tokens[i + 2]
+            print(add(num1, num2))
